d7d33682de208aa48a8ec817fa9c654dcaeee20f
April Adams <illusionlace@gmail.com>
2020-09-18 16:39:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
-class Person 
+class Person
   def initialize(name)
-    @name = name 
-  end 
+    @name = name
+  end
   def name=(name)
-    @name = name 
-    def name 
-      @name 
+    @name = name
+    def name
+      @name
     end 
-end 
+end
