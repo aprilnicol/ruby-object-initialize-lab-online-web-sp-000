8d480540457506634ad5ed13682d50cbcacd8c05
April Adams <illusionlace@gmail.com>
2020-09-18 16:48:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed, "Mutt")
     @name = name
     @breed = breed
   end
@@ -11,7 +11,7 @@ class Dog
     @name
   end
   def breed=(breed)
-    @breed = "Mutt"
+    @breed = breed 
 end
 
 def breed
